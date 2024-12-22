3a84eff145bf0ee5ff204c8c0accdbfffc3fd141
zxbuzzy <zxbuzzy@gmail.com>
2024-12-22 19:46:09 +0300

    add nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,35 @@
+require("config.lazy")
+require("config.colorscheme")
+require("config.telescope")
+require("config.treesitter")
+require("config.lualine")
+require("config.mason")
+require("config.mason-lspconfig")
+require("config.nvim-lspconfig")
+require("config.none-ls")
+
+-- change the leader key
+vim.g.mapleader = " "
+
+-- remap vim keys
+vim.keymap.set('i', 'jj', '<ESC>')
+
+-- navigation keys
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+-- general settings
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set softtabstop=2")
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+-- neotree specific
+vim.keymap.set('n', '<leader>n', ':Neotree reveal left<CR>')
+
+
---
+++ .config/nvim/lua/config/nvim-lspconfig.lua
@@ -0,0 +1,7 @@
+local lspconfig = require("lspconfig")
+lspconfig.lua_ls.setup({})
+lspconfig.pyright.setup({})
+
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
