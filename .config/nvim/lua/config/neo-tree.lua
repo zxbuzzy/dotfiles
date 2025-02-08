514e4e6987d2085cea5e8e7e354646130100731f
zxbuzzy <zxbuzzy@gmail.com>
2025-02-08 14:57:03 +0400

    minor changes at neotree config file

--- .config/nvim/lua/config/neo-tree.lua
+++ .config/nvim/lua/config/neo-tree.lua
@@ -1,9 +1,9 @@
 require('neo-tree').setup {
   filesystem = {
     filtered_items = {
-      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      visible = true, 
       hide_dotfiles = false,
-      hide_gitignored = true,
+      hide_gitignored = false,
     }
 	}
 }
