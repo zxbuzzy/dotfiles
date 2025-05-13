217ff8d830a8cdad712479db663b5cec25fb46b0
zxbuzzy <zxbuzzy@gmail.com>
2025-05-13 05:59:30 +0300

    change theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,11 +1,3 @@
 return {
-  { "ellisonleao/gruvbox.nvim" },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
-  },
+  opts = { style = "moon" },
 }
